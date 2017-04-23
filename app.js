df75739273ff0b1bc08fea1e4a0e892990fd3262
John Hampton <john_j_hampton@yahoo.com>
2017-04-22 23:07:08 -0400

    Remove unused route (users)

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var catalog = require('./routes/catalog');
 
 var app = express();
